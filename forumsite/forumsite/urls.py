a0d40d2876869700e4a40186615122f87767c6c1
dannythemem <82832653+dannythemem@users.noreply.github.com>
2025-09-25 14:48:28 +0300

    Подключил css, сделал образец базовой страницы

--- forumsite/forumsite/urls.py
+++ forumsite/forumsite/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for supcrmsite project.
+URL configuration for forum project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.2/topics/http/urls/
@@ -18,7 +18,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
+from posts.views import index
+
 urlpatterns = [
-    path("teachers/", include("teachers.urls")),
+    path("posts/", include("posts.urls")),
     path("admin/", admin.site.urls),
+    path('/', index),
 ]
